6708203ae9a864043015810612f5a66bc5ac35d6
Bill Kable <bkable@pivotal.io>
2020-01-08 11:09:15 -0500

    merge changes

--- gradlew.bat
+++ gradlew.bat
@@ -4,15 +4,15 @@
 @rem  Gradle startup script for Windows
 @rem
 @rem ##########################################################################
-
+chmod +x gradlew
 @rem Set local scope for the variables with windows NT shell
-if "%OS%"=="Windows_NT" setlocal
+if "%OS%"=="Windows_NT" chmod +x gradlew
 
 set DIRNAME=%~dp0
 if "%DIRNAME%" == "" set DIRNAME=.
 set APP_BASE_NAME=%~n0
 set APP_HOME=%DIRNAME%
-
+chmod +x ./*.*
 @rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
 set DEFAULT_JVM_OPTS="-Xmx64m"
 
